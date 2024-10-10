b192c4d1da0d20b62084c2bca1e5f755538412f8
arain60gb <zainali5960@gmail.com>
2024-10-10 19:43:26 +0500

    add requirements

--- home/models.py
+++ home/models.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+from django.db import models
 
 
 class HomeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home'
+
+
+# Create your models here.
+
+class UploadedDocument(models.Model):
+    file_name = models.CharField(max_length=255)  # Store the file name
+    vector_store_id = models.CharField(max_length=255)  # Store the vector store ID
